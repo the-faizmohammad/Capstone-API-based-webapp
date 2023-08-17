26176192e01615e42d7e05ff4e140a889556e449
Hossain <arafat.kd99@gmail.com>
2023-08-17 10:10:46 +0100

    Update getLike function in InvolvementAPI.js"

--- src/components/InvolvementAPI.js
+++ src/components/InvolvementAPI.js
@@ -1,18 +1,31 @@
 const API_BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 const APP_ID = 'kWOTV5VjYokcGzg5Fbds';
+
 // Function to record a like for a specific item
 export async function recordLike(itemId) {
-    const response = await fetch(`${API_BASE_URL}apps/${APP_ID}/likes/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ item_id: itemId }),
-    });
-  
-    if (response.status === 201) {
-      return true; // Like recorded successfully
-    } else {
-      return false; // Like recording failed
-    }
+  const response = await fetch(`${API_BASE_URL}apps/${APP_ID}/likes/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: itemId }),
+  });
+
+  if (response.status === 201) {
+    return true; // Like recorded successfully
+  } else {
+    return false; // Like recording failed
   }
+}
+
+// Function to get a list of items with respective likes
+export async function getLikes() {
+  const response = await fetch(`${API_BASE_URL}apps/${APP_ID}/likes/`);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    return null;
+  }
+}
