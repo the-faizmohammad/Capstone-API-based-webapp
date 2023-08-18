f5a9de3ceda95b61244d5ffd3ce02ded20da4e85
Faiz Mohammad <email2faizm@gmail.com>
2023-08-18 16:10:51 +0530

    Adding showPopup function in PopupModule.js

--- src/components/PopupModule.js
+++ src/components/PopupModule.js
@@ -1,1 +1,10 @@
+import InvolvementAPI from './InvolvementAPI.js';
 
+const showPopup = async (show) => {
+  // Create and style the popup container
+  const popupContainer = document.createElement('div');
+  popupContainer.classList.add('popup-container');
+
+  // Fetch details about the selected show from TVmaze API
+  const response = await fetch(`https://api.tvmaze.com/shows/${show.id}`);
+  const showDetails = await response.json();
