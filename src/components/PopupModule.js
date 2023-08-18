726c5bcc075a2aaa28826fb283575ac1ff5e11f8
Faiz Mohammad <email2faizm@gmail.com>
2023-08-18 16:11:58 +0530

    Adding Popup content in PopupModule.js

--- src/components/PopupModule.js
+++ src/components/PopupModule.js
@@ -8,3 +8,24 @@ const showPopup = async (show) => {
   // Fetch details about the selected show from TVmaze API
   const response = await fetch(`https://api.tvmaze.com/shows/${show.id}`);
   const showDetails = await response.json();
+// Populate the popup content with show details
+  popupContainer.innerHTML = `
+    <div class="popup-header">
+      <img src="${showDetails.image.medium}" alt="${showDetails.name}">
+      <h2>${showDetails.name}</h2>
+    </div>
+    <div class="popup-content">
+      <p>${showDetails.summary}</p><br>
+      <a href="${show.url}" target="_blank">Learn More</a>
+    </div>
+    <button class="popup-close-button">✖</button>
+    <div class="comment-form">
+      <input type="text" id="commentName" placeholder="Your Name">
+      <textarea id="commentText" placeholder="Your Comment"></textarea>
+      <button class="popup-comment-button">💬 Add Comment</button>
+    </div>
+    <div class="comment-count">Comments (0)</div>
+    <div class="comment-list">
+      <!-- Comment list will be populated here -->
+    </div>
+  `;
