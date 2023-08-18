0292d12199d50ff0188ed94455b05eaff4720219
user-faiz <email2faizm@gmail.com>
2023-08-18 16:44:46 +0530

    Update InvolvementAPI.js

--- src/components/InvolvementAPI.js
+++ src/components/InvolvementAPI.js
@@ -28,7 +28,36 @@ const getLikes = async () => {
   return null;
 };
 
+// Function to record a comment for a specific item
+const recordComment = async (itemId, username, comment) => {
+  const response = await fetch(`${API_BASE_URL}apps/${APP_ID}/comments/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: itemId, username, comment }),
+  });
+
+  if (response.status === 201) {
+    return true; // Comment recorded successfully
+  }
+  return false; // Comment recording failed
+};
+
+// Function to get a list of comments for a specific item
+const getComments = async (itemId) => {
+  const response = await fetch(`${API_BASE_URL}apps/${APP_ID}/comments?item_id=${itemId}`);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  }
+  return null;
+};
+
 export default {
   recordLike,
   getLikes,
+  recordComment,
+  getComments,
 };
