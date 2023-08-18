9ad44cd02230c1e9605eae0714e978818f979131
user-faiz <email2faizm@gmail.com>
2023-08-19 00:10:03 +0530

    Define mock data in test ,js

--- src/components/test.js
+++ src/components/test.js
@@ -2,3 +2,8 @@ import InvolvementAPI from './InvolvementAPI.js';
 
 // Mock the fetch function
 global.fetch = jest.fn();
+// Define mock data for likes and comments
+const mockLikesData = [
+  { item_id: 123, likes: 5 },
+  { item_id: 456, likes: 10 },
+];
